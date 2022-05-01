360057ad9e5b249f8cc0c77d8c01ca8497c92145
BenAyedMehdi <mehdibenayed97@gmail.com>
2022-05-01 18:54:50 +0200

    show dialog when click on edit

--- src/app/components/view-movie/view-movie.component.ts
+++ src/app/components/view-movie/view-movie.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
 import { Movie } from 'src/app/Movie';
+import { AddMovieComponent } from '../add-movie/add-movie.component';
 import { MovieComponent } from '../movie/movie.component';
 
 @Component({
@@ -10,6 +15,7 @@ import { MovieComponent } from '../movie/movie.component';
 })
 export class ViewMovieComponent implements OnInit {
   constructor(
+    public dialog: MatDialog,
     public dialogRef: MatDialogRef<MovieComponent>,
     @Inject(MAT_DIALOG_DATA) public movie: Movie
   ) {}
@@ -23,6 +29,22 @@ export class ViewMovieComponent implements OnInit {
 
   onDelete(movie: Movie) {
     return movie;
+  }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(AddMovieComponent, {
+      height: '95%',
+      width: '80%',
+      data: this.movie,
+    });
+    /*
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        console.log('result in movie: ', result);
+        this.movieToDelete = result;
+        this.deleteMovieEvent.emit(this.movie);
+      }
+    });*/
   }
 
   rates = Object.keys(this.movie.ratings).map((key) => {
